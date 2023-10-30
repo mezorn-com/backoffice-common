ce4003e73f1d0f5d9af07e8ce28bd63ce185719b
Jonondarnad <bjonondarnad@gmail.com>
2023-10-30 16:26:18 +0800

    code splitting and refactoring

--- components/table/TableContextProvider.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { createMyStore, MyContext } from '@/backoffice-common/components/table/context';
-
-const MyProvider = ({ children }: { children: React.ReactNode }) => {
-    const store = createMyStore();
-    return <MyContext.Provider value={store}>{children}</MyContext.Provider>;
-};
-
-export default MyProvider;

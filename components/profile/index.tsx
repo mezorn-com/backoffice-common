e27df6158323a28f522b31949b557aa7828bc2b7
Jonondarnad <bjonondarnad@gmail.com>
2024-03-21 18:30:02 +0800

    added option to add custom config items

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -4,15 +4,19 @@ import { IconSettings, IconUser } from '@tabler/icons-react';
 import classes from './Profile.module.scss';
 import useStore from '@/store';
 import { useTranslation } from 'react-i18next';
+import { useConfigItems } from '@/lib/config-items';
 
 const Profile = () => {
 
 	const { t } = useTranslation();
+	// @ts-ignore
+	const { items, render } = useConfigItems();
 	const userName = useStore(state => state.auth.name);
 	const clearStore = useStore(state => state.clearStore);
 
 	return (
 		<div className={classes.container}>
+			{render()}
 			<Flex
 				justify="center"
 				align="center"
@@ -34,6 +38,15 @@ const Profile = () => {
 				</Menu.Target>
 
 				<Menu.Dropdown>
+					{
+						items.map(item => {
+							return (
+								<Menu.Item key={item.key} onClick={item.onClick}>
+									{item.label}
+								</Menu.Item>
+							)
+						})
+					}
 					<Menu.Item onClick={clearStore}>{t('logout', { ns: 'auth' })}</Menu.Item>
 				</Menu.Dropdown>
 			</Menu>

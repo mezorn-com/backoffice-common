0e5aeba4ef42170f80f19c079aa540630667dff6
temuujin3 <temuujin@mezorn.com>
2024-08-09 15:22:48 +0800

    IMatchAddressPicker type import fix

--- components/form/components/map-address-picker/index.tsx
+++ components/form/components/map-address-picker/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import axios from 'axios';
 import qs from 'qs';
 import { TextInput, Button, Modal, Text } from '@mantine/core';
-import { MapAddressPicker as IMapAddressPicker } from '@/backoffice-common/types/form';
+import { IMapAddressPicker } from '@/backoffice-common/types/form';
 import type { IResponse } from '@/backoffice-common/types/api';
 import { MAP_INITIAL_ZOOM, MAP_DEFAULT_STARTING_POINT, MAP_MAX_ZOOM, MAP_MIN_ZOOM } from '@/config';
 import { MapContainer, ZoomControl } from 'react-leaflet';
